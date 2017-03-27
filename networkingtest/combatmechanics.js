c88a9e8c7d2f0df44eb9e8aead5d21946cacc010
Alex Herdzik <alex.j.herdzik@gmail.com>
2017-03-26 17:08:32 -0700

    Stripped out all unecessary crap

--- networkingtest/combatmechanics.js
+++ networkingtest/combatmechanics.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
 //Constants
 var CANVAS_WIDTH = 1280;
 var CANVAS_HEIGHT = 720;
+var player_location_x = CANVAS_WIDTH/2;
+var player_location_y = CANVAS_HEIGHT/2;
 var FPS = 60;
 var player;
 var gamestate = 0;
@@ -23,7 +25,8 @@ var player;
 images = {};
 var mouse = {};
 
-function init(){
+function init()
+{
 	var canvasElement = document.querySelector("canvas");
 	canvasElement.width = CANVAS_WIDTH;
 	canvasElement.height = CANVAS_HEIGHT;
@@ -40,9 +43,8 @@ function init(){
 	window.addEventListener("keyup", function(e)
     {  
         inputHandler.keyUp(e.keyCode);
-	}
-	);
-	player= new Player(300,100,50,1);
+    });
+	player= new Player(CANVAS_WIDTH/2,CANVAS_HEIGHT,50,1);
 }//end init
 
 function track_mouse(e)
@@ -62,85 +64,66 @@ function loop(){
 
 
 function update(){
-	//console.log("player.x: " + player.x + "player.y: " + player.y);
-	//console.log("player2.x: " + player2.x + "player2.y: " + player2.y);
-	
-	if(player.HP>0){
-		if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_LEFT]){
-			player.xVel-=(10); 			
-		}
-		if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_RIGHT]){
-			player.xVel+=(10); 
-		}
-		if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_UP]){
-			player.yVel+=(-20);
-		}
-		if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_DOWN]){
-			player.yVel+=(20);	
-		}
-        
-		checkCollisions();
-		handleCollisions();
-		player.update();
-		inputHandler.update();
-	}
-	
-	
+	console.log("player.x: " + player.xVel + "player.y: " + player.yVel);
+    
+    if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_LEFT])
+    {
+        player.xVel-=(10); 			
+    }
+    if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_RIGHT])
+    {
+        player.xVel+=(10); 
+    }
+    if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_UP])
+    {
+        player.yVel+=(-10);
+    }
+    if(inputHandler.keydown[inputHandler.KEYBOARD.KEY_DOWN])
+    {
+        player.yVel+=(10);	
+    }
+    player.update();
 }//END UPDATE
 
-function draw(){
-ctx.save();
-ctx.clearRect (0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-drawBackground();
-
-player.draw();
-drawBars();
-ctx.restore();
+function draw()
+{    
+    ctx.save();
+    ctx.clearRect (0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    drawBackground();
+    player.draw();
+    ctx.restore();
 }//END DRAW
 
-function drawBackground(){
+function drawBackground()
+{
 	ctx.fillStyle="#CCCCCC";
 	ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
 }
 
-function drawBars(){
-	ctx.fillStyle="#FF0000";
-	ctx.fillRect(10,10,player.HP*4,20);
-	ctx.fillStyle="#FFB90F";
-	ctx.fillRect(10,40,player.energy*2,20);
-}
-
-function handleCollisions(){
-player.keepInBounds();
-}
-
-
 function loadImages(){
-		var numLoadedImages =0;
-		var numImages = 0;
-		//get num of sources 
-			for(var imageName in IMAGE_SOURCES){
-				numImages++;
-			}
-			
-		//load images
-		for(var imageName in IMAGE_SOURCES){
-			images[imageName] = new Image();
-			images[imageName].src = IMAGE_SOURCES[imageName];
-			
-			images[imageName].onload = function(){
-			if(++numLoadedImages >= numImages){
-			console.log("DONE LOADING IMAGES");
-			init();
-			
-			}
-		}
+    var numLoadedImages =0;
+    var numImages = 0;
+    //get num of sources 
+    for(var imageName in IMAGE_SOURCES)
+    {
+        numImages++;
+    }
+        
+    //load images
+    for(var imageName in IMAGE_SOURCES)
+    {
+        images[imageName] = new Image();
+        images[imageName].src = IMAGE_SOURCES[imageName];
+            
+        images[imageName].onload = function()
+        {
+            if(++numLoadedImages >= numImages)
+            {
+                console.log("DONE LOADING IMAGES");
+                init();
+            }
+        }
 	}
-}
-
-function checkCollisions()
-{
-	
 }
 
 function rotate(x,y,sin,cos,reverse)
@@ -157,10 +140,12 @@ function rotate(x,y,sin,cos,reverse)
 		result.y = (y * cos) + (x * sin);
 	}
 	
-	if(result.x<0.001 && result.x>-0.001){
+	if(result.x<0.001 && result.x>-0.001)
+    {
 		result.x=0;
 	}
-	if(result.y<0.001 && result.y>-0.001){
+	if(result.y<0.001 && result.y>-0.001)
+    {
 		result.y=0;
 	}
 	return result;
--- networkingtest/inputhandler.js
+++ networkingtest/inputhandler.js
@@ -16,173 +16,25 @@ this.KEYBOARD = {
 		
 		};
 this.player1input= [];
-this.player2input= [];
 	this.keydown = [];
 };
 			
 InputHandler.prototype.keyDown= function(key){
 //console.log(key);
 if(!this.keydown[key]){
-	if(key==65 || key==87 || key==83 || key==68 || key==81){
+	if(key==65 || key==87 || key==83 || key==68 || key==81)
+    {
 		this.player1input.push(new InputTimer(key));
-	}
-	else if((key >=37 && key <=40) || key==96){
-		this.player2input.push(new InputTimer(key));
 	}
 }
 this.keydown[key]=true;
 };
 
-InputHandler.prototype.keyUp= function(key){
-this.keydown[key]=false;
+InputHandler.prototype.keyUp= function(key)
+{
+    this.keydown[key]=false;
 };
 
-InputHandler.prototype.update= function(){
-	//console.log(this.player1input. length + " " + this.player2input.length);
-	for(var i = 0; i <this.player1input.length; i++){
-	this.player1input[i].timer+=(FPS/2500);
-		if(this.player1input[i].timer>0.3){
-			this.player1input.splice(i,1);
-		}
-	}
-
-	for(i = 0; i <this.player2input.length; i++){
-	this.player2input[i].timer+=(FPS/2500);
-		if(this.player2input[i].timer>0.3){
-			this.player2input.splice(i,1);
-		}
-	}
-
+InputHandler.prototype.update= function()
+{
 }
-
-InputHandler.prototype.player1DoubleTap = function(){
-	if(this.player1input.length>=2){
-		if(this.player1input.length==4){
-			var tally5=0, tally6=0, tally7=0,tally8=0;
-			for(var i=0; i<this.player1input.length; i++){
-				var curkey=this.player1input[i].key;
-				switch(curkey){
-					case 65:
-					tally5++;
-					break;
-					case 83:
-					tally6++;
-					break;
-					case 68:
-					tally7++;
-					break;
-					case 87:
-					tally8++;
-					break;
-				}
-			}
-			if(tally5==2){
-				if(tally8==2){
-					this.player1input.splice(0,4);
-					return 5;
-				}else if(tally6==2){
-					this.player1input.splice(0,4);
-					return 6;
-				}
-			}else if(tally7==2){
-				if(tally8==2){
-					this.player1input.splice(0,4);
-					return 8;
-				}else if(tally6==2){
-					this.player1input.splice(0,4);
-					return 7;
-				}
-			
-			}
-		}
-	
-		if(this.player1input[0].key==65 && this.player1input[1].key==65){
-			return 1;
-			player1input.splice(0,2);
-		}
-		if(this.player1input[0].key==83 && this.player1input[1].key==83){
-			return 2;
-			player1input.splice(0,2);
-		}
-		if(this.player1input[0].key==68 && this.player1input[1].key==68){
-			return 3;
-			player1input.splice(0,2);
-		}
-		if(this.player1input[0].key==87 && this.player1input[1].key==87){
-			return 4;
-			player1input.splice(0,2);
-		}
-	}
-	return 0;
-}
-
-InputHandler.prototype.player2DoubleTap = function(){
-	
-	if(this.player2input.length>=2){
-		if(this.player2input.length==4){
-			
-			var tally5=0, tally6=0, tally7=0,tally8=0;
-			for(var i=0; i<this.player2input.length; i++){
-				var curkey=this.player2input[i].key;
-				switch(curkey){
-					case 37:
-					tally5++;
-					break;
-					case 40:
-					tally6++;
-					break;
-					case 39:
-					tally7++;
-					break;
-					case 38:
-					tally8++;
-					break;
-				}
-			}
-			if(tally5==2){
-				if(tally8==2){
-					this.player2input.splice(0,4);
-					return 5;
-				}else if(tally6==2){
-					this.player2input.splice(0,4);
-					return 6;
-				}
-			}else if(tally7==2){
-				if(tally8==2){
-					this.player2input.splice(0,4);
-					return 8;
-				}else if(tally6==2){
-					this.player2input.splice(0,4);
-					return 7;
-				}
-			
-			}
-		}
-		if(this.player2input[0].key==37 && this.player2input[1].key==37){
-			return 1;
-			player2input.splice(0,2);
-		}
-		if(this.player2input[0].key==40 && this.player2input[1].key==40){
-			return 2;
-			player2input.splice(0,2);
-		}
-		if(this.player2input[0].key==39 && this.player2input[1].key==39){
-			return 3;
-			player2input.splice(0,2);
-		}
-		if(this.player2input[0].key==38 && this.player2input[1].key==38){
-			return 4;
-			player2input.splice(0,2);
-		}
-	
-	}
-	return 0;
-}
-
-InputHandler.prototype.printDoubleTap = function(){
-	for(var i = 0; i <this.player1input.length; i++){
-	}
-	for(i = 0; i <this.player2input.length; i++){
-	}
-}
-
